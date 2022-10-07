8961302fc79aca99993a76fb99d21f1b169b69d5
juliovianadev <juliovianadev@gmail.com>
2022-10-07 01:32:23 -0300

    model product categoria fazendo o através para conectar duas tabelas

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,7 @@ class Product < ApplicationRecord
   validates :description, presence: true
   validates :price,presence: true,numericality: {greater_than: 0}
   validates :name,presence: true, uniqueness: {case_sensitive: false}
+  
+  has_many :product_categories, dependent: :destroy
+  has_many :categories,through: :product_categories
 end
