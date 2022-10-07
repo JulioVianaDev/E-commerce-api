d7568b9f2f73d489044d29c4535e1acaf11b1ab0
juliovianadev <juliovianadev@gmail.com>
2022-10-07 01:10:15 -0300

    testes unitarios com model de jogos concluido

--- app/models/system_requirement.rb
+++ app/models/system_requirement.rb
@@ -1,4 +1,6 @@
 class SystemRequirement < ApplicationRecord
   validates :operational_system,:storage,:processor,:memory,:video_board, presence: true
   validates :name,presence: true, uniqueness: {case_sensitive: false}
+
+  has_many :games,dependent: :restrict_with_error 
 end
