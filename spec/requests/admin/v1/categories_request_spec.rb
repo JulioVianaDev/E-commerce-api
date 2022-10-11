a2e62546f63f6698bd02fa185b0453873782cacb
juliovianadev <juliovianadev@gmail.com>
2022-10-11 16:58:07 -0300

    testes do category ok

--- spec/requests/admin/v1/categories_request_spec.rb
+++ spec/requests/admin/v1/categories_request_spec.rb
@@ -15,5 +15,52 @@ RSpec.describe "Admin V1 Categories", type: :request do
     end
   end
 
+  context "POST /categories" do
+    let(:url) { "/admin/v1/categories" }
+
+    context "with valid params" do
+      let(:cateory_params) { {category: attributes_for(:category )}.to_json }
+      
+      it "adds a new Category" do
+        expect do
+          post url , headers: auth_header(user),params: cateory_params
+        end.to change(Category, :count).by(1) 
+      end
+
+      it 'returns last added Category' do
+        post url, headers: auth_header(user),params: cateory_params
+        expect_category = Category.last.as_json(only: %i(id name))
+        expect(body_json['category']).to eq expect_category
+      end
+
+      it "returns success status" do
+        post url, headers: auth_header(user),params: cateory_params
+        expect(response).to have_http_status(:ok)
+      end 
+    end
+
+    context "with valid params" do
+      let(:category_invalid_params) do
+        {category: attributes_for(:category, name: nil)}.to_json
+      end
+
+      it 'does not add a category ' do
+        expect do
+          post url,headers: auth_header(user), params: category_invalid_params
+        end.to_not change(Category, :count)
+      end
+
+      it 'returns error messages' do
+        post url,headers: auth_header(user), params: category_invalid_params
+        expect(body_json['errors']['fields']).to have_key('name')
+      end
+
+      it 'returns unprocessable_entity' do
+        post url,headers: auth_header(user), params: category_invalid_params
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
+
   
 end
