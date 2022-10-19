4ac8947c08c75c44f8b9eea57d5d8a5c7dd9323f
juliovianadev <juliovianadev@gmail.com>
2022-10-19 16:54:55 -0300

    adicionando status no product

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,13 +4,13 @@ class Product < ApplicationRecord
   include Paginatable
   
   belongs_to :productable, polymorphic: true
-  validates :description, presence: true
+  validates :description,:status, presence: true
   validates :price,presence: true,numericality: {greater_than: 0}
   validates :name,presence: true, uniqueness: {case_sensitive: false}
   validates :image,presence: true 
   has_many :product_categories, dependent: :destroy
   has_many :categories,through: :product_categories
-
+  enum status: { avainable: 1,unavainable: 2}
   has_one_attached :image
   validates :image, presence: true
 end
